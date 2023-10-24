fc2a391f0867b283a8c334207e9f690b5d2315b8
acapela <andrew@Capelagames.com.au>
2023-10-24 16:14:31 +1100

    Move and jumping, not resetting on landing

--- Source/rollerball/Private/RollerBallPlayer.cpp
+++ Source/rollerball/Private/RollerBallPlayer.cpp
@@ -30,12 +30,63 @@ ARollerBallPlayer::ARollerBallPlayer()
 // Called when the game starts or when spawned
 void ARollerBallPlayer::BeginPlay()
 {
+	Mesh->OnComponentHit.AddDynamic(this, &ARollerBallPlayer::OnHit);
+	
 	Super::BeginPlay();
+
+	//Account for mass in moveforce
+	MoveForce *= Mesh->GetMass();
+	JumpImpulse *= Mesh->GetMass();
+	
 }
+
+
+
 
 // Called to bind functionality to input
 void ARollerBallPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	InputComponent->BindAxis("MoveForward", this, &ARollerBallPlayer::MoveForward);
+	InputComponent->BindAxis("MoveRight", this, &ARollerBallPlayer::MoveRight);
+	InputComponent->BindAction("Jump", IE_Pressed, this , &ARollerBallPlayer::Jump);
+	//InputComponent->BindAction("Jump", IE_Released, this , &ARollerBallPlayer::Jump);
 }
 
+void ARollerBallPlayer::MoveRight(float Value)
+{
+	const FVector Right = Camera->GetRightVector() * MoveForce * Value;
+	Mesh->AddForce(Right);
+}
+
+void ARollerBallPlayer::MoveForward(float Value)
+{
+	const FVector Forward = Camera->GetForwardVector() * MoveForce * Value;
+	Mesh->AddForce(Forward);
+
+	//GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Orange,
+	//	FString::Printf(TEXT("forward pressed")));
+
+	//UE_LOG(LogTemp, Warning, TEXT("Hello"))
+	
+}
+
+void ARollerBallPlayer::Jump()
+{
+	if(JumpCount >= MaxJumpCont){return;}
+	Mesh->AddForce(FVector(0,0,JumpImpulse));
+	JumpCount++;
+}
+
+void ARollerBallPlayer::OnHit(UPrimitiveComponent* HitComponent,
+	AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse,
+	const FHitResult& Hit )
+{
+	float HitDirection = Hit.Normal.Z;
+
+	if(HitDirection > 0)
+	{
+		JumpCount = 0;
+	}
+}
--- Source/rollerball/Public/RollerBallPlayer.h
+++ Source/rollerball/Public/RollerBallPlayer.h
@@ -40,6 +40,8 @@ protected:
 	float JumpImpulse = 500.0f;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	int32 MaxJumpCont = 1;
+
+	int32 JumpCount = 0;
 	
 
 public:	
@@ -47,4 +49,13 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+	void MoveRight(float Value);
+	void MoveForward(float Value);
+	void Jump();
+	
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent,
+		AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse,
+		const FHitResult& Hit );
 };
